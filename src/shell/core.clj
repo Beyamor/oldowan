84af1e00189c8054e72d292e3b1c4884185bda0b
Beyamor <beyamor@gmail.com>
2012-10-13 16:00:05 -0700

    Cool. Got us a little read-eval-print loop.

--- src/shell/core.clj
+++ src/shell/core.clj
@@ -1,6 +1,44 @@
-(ns shell.core)
+(ns shell.core
+  (:import [java.io File]))
+
+(def current-dir (atom nil))
+
+(def prompt-terminator "೯ ")
+
+(defn print-prompt
+  "Prints the command prompt string."
+  []
+  (let [path-name (.getAbsolutePath @current-dir)]
+    (print (str path-name prompt-terminator))
+    (flush)))
+
+(defn get-command
+  "Reads a command from standard input."
+  []
+  (read-line))
+
+(defn process-command
+  "Does *something* with a command."
+  [command]
+  (println (str " -> " command)))
+
+(defn start-read-loop
+  "That thing that does the things."
+  []
+  (loop []
+    (do
+      (print-prompt)
+      (let [command (get-command)]
+        (process-command command)
+        (recur)))))
+
+(defn setup
+  "Initializes whatevs out little shelly needs."
+  []
+  (reset! current-dir (File. (System/getProperty "user.dir"))))
 
 (defn -main
   "Our cute little main method."
   [& args]
-  (println "hello world"))
+  (setup)
+  (start-read-loop))
