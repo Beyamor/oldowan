5c8609e0a4bde0970a244287ebbed643209894c5
Beyamor <beyamor@gmail.com>
2012-10-13 17:00:54 -0700

    Made all commands multimethods.

--- src/shell/core.clj
+++ src/shell/core.clj
@@ -4,9 +4,6 @@
 (def working-directory (atom nil))
 
 (def prompt-terminator "೯ ")
-(def shell-commands
-  {"pwd" :print-working-directory
-   "c-and-a" :print-command-and-args})
 
 (defn get-working-directory
   "Returns the working directory as a Java file."
@@ -50,24 +47,26 @@
   [command]
   (println (str "invalid command: " command)))
 
-(defmulti execute-shell-command (fn [command & args] (keyword command)))
+(defmulti execute-command (fn [command & args] command))
 
-(defmethod execute-shell-command :print-working-directory
+(defmethod execute-command "pwd"
   [command & args]
   (println (get-working-directory-name)))
 
-(defmethod execute-shell-command :print-command-and-args
+(defmethod execute-command "command-and-args"
   [command & args]
   (println (str "Command: " command))
   (doall (map-indexed (fn [index arg] (println (str "Arg" index ": " arg))) args)))
+
+(defmethod execute-command :default
+  [command & args]
+  (print-invalid-command command))
 
 (defn process-command
   "Does *something* with a command."
   [command & args]
   (if (not (empty? command))
-    (if-let [shell-command (shell-commands command)]
-      (apply execute-shell-command shell-command args)
-      (print-invalid-command command))))
+    (apply execute-command command args)))
 
 (defn start-read-loop
   "That thing that does the things."
